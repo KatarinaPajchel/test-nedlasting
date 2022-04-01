d3fd77cf819831a49bf8c026d1c61a94363bcdfc
KatarinaPajchel <102804966+KatarinaPajchel@users.noreply.github.com>
2022-04-01 15:08:10 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
